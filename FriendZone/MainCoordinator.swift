a2d5a24595718c82a120d136a85ef26d2a06e430
shubhamaydas1@gmail.com <shubhamaydas1@gmail.com>
2022-04-20 15:10:58 +0200

    Inital Commit

---
+++ FriendZone/MainCoordinator.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = ViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+
+    func configure(friend: Friend) {
+        let vc = FriendViewController.instantiate()
+
+        vc.coordinator = self
+        vc.friend = friend
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+    func update(friend: Friend) {
+        guard let vc = navigationController.viewControllers.first as? ViewController else {
+            return
+        }
+
+        vc.update(friend: friend)
+    }
+}
---
+++ FriendZone/TextTableViewCell.swift
@@ -0,0 +1,18 @@
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+    @IBOutlet var textField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
